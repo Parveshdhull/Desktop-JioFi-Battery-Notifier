8426fe50a30ba766717eb3840aac73c808430985
parveshdhull <parvesh.dhullmonu@gmail.com>
2020-12-10 03:29:13 +0530

    Added Error Messages

--- jiofi_once.py
+++ jiofi_once.py
@@ -1,17 +1,22 @@
-#!/bin/python3
+#!/bin/python
 
-import requests, re, time
+import requests, re
 from pynotifier import Notification
 
 
-# Change Jiofi Address if Required
-url = "http://192.168.225.1"
-response = requests.get(url, timeout=1)
+try:
 
-# Finding Battery Percentage From Response
-pattern = r'(id="batterylevel" value=")(\d*)(%")'
-matches = re.findall(pattern, response.text)
-percentage = matches[0][1]
+	# Change Jiofi Address if Required
+	url = "http://192.168.225.1"
+	response = requests.get(url, timeout=1)
 
-Notification(title='JioFi Battery Notifier',description='Battery Level ' + percentage, duration=5).send()
+	# Finding Battery Percentage From Response
+	pattern = r'(id="batterylevel" value=")(\d*)(%")'
+	matches = re.findall(pattern, response.text)
+	percentage = matches[0][1]
+
+	Notification(title='JioFi Battery Notifier',description='Battery Level ' + percentage, duration=5).send()
+
+except Exception as e:
+       	Notification(title='JioFi Battery Notifier',description='JioFi Not Connected', duration=5).send()
 
